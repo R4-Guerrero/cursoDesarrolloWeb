b75d2bc9bc6c257e819f7472e87fe8d24d3ed403
R4-Guerrero <aguerrero.nolasco@gmail.com>
2023-08-05 00:18:54 -0600

    Se agrego archivo JS

---
+++ variables.js
@@ -0,0 +1,17 @@
+var nombre = "Arturo";
+var edad = 47;
+var altura = 1.69;
+
+let esMayorDeEdad = true;
+
+if(esMayorDeEdad) {
+    console.log("Si eres mayor de esdad puedes entrar al bar");
+}else {
+    console.log("No eres mayor de edad lo siento no puedes entrar al bar")
+}
+
+let persona = { // Esto es un bloque donde se almacenan las caracteristicas del objeto persona
+    nombre: "Juan",
+    edad: 30,
+    ciudad: Monterrey
+};
